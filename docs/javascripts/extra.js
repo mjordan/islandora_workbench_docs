803f0f42695a9b9373289f9c55ff7c047c2f45a2
Joe Corall <joe@libops.io>
2023-11-20 15:36:25 -0500

    Update extra.js

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,6 +1,7 @@
+// fully expand the open api schema
 (() => {
   const i = setInterval(() => {
-    [...document.querySelectorAll('[aria-expanded="false"]')].map((el) =>
+    [...document.querySelectorAll('.swagger-ui [aria-expanded="false"]')].map((el) =>
       el.click()
     ).length || clearInterval(i);
   }, 1000);
